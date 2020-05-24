2e7395aed670a9c9e152fd850d9a202f15eb23d4
MichalSkapski <mskapski@edu.cdv.pl>
2020-05-24 22:36:35 +0200

    Optymazing the code, adding dirty scoreboard script, adding script, which save the name of user from the "NameAssignerScene"

---
+++ Assets/scripts/Highscore/NameAssignerScene.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class NameAssignerScene : MonoBehaviour
+{
+    public TMP_InputField userNameBox;
+    public string userNewName;
+    private string _menuScene = "Menu";
+
+    private void Start()
+    {
+        GetName();
+    }
+
+
+    public void GetName()
+    {
+
+    }
+
+    public void ClickSaveButton()
+    {
+        PlayerPrefs.SetString(userNewName, userNameBox.text);
+        SceneManager.LoadScene(_menuScene);
+        Debug.Log("Your name is: " + PlayerPrefs.GetString(userNewName));
+    }
+}
---
+++ Assets/scripts/MenuScripts/Scoreboard.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoreboard : MonoBehaviour
+{
+    private int _gameNumber=3;
+    public GameObject[] _posNumberArray;
+    public GameObject[] _nameArray;
+    public GameObject[] _scoreArray;
+
+    private void Awake()
+    {
+        _posNumberArray[0].SetActive(false);
+        _posNumberArray[1].SetActive(false);
+        _posNumberArray[2].SetActive(false);
+        _posNumberArray[3].SetActive(false);
+        _posNumberArray[4].SetActive(false);
+        _posNumberArray[5].SetActive(false);
+        _nameArray[0].SetActive(false);
+        _nameArray[1].SetActive(false);
+        _nameArray[2].SetActive(false);
+        _nameArray[3].SetActive(false);
+        _nameArray[4].SetActive(false);
+        _nameArray[5].SetActive(false);
+        _scoreArray[0].SetActive(false);
+        _scoreArray[1].SetActive(false);
+        _scoreArray[2].SetActive(false);
+        _scoreArray[3].SetActive(false);
+        _scoreArray[4].SetActive(false);
+        _scoreArray[5].SetActive(false);
+    }
+
+    public void AddingNewScore()
+    {
+        switch (_gameNumber)
+        {
+            case 1:
+                _posNumberArray[0].SetActive(true);
+                _nameArray[0].SetActive(true);
+                _scoreArray[0].SetActive(true);
+                break;
+            case 2:
+                _posNumberArray[0].SetActive(true);
+                _nameArray[0].SetActive(true);
+                _scoreArray[0].SetActive(true);
+                _posNumberArray[1].SetActive(true);
+                _nameArray[1].SetActive(true);
+                _scoreArray[1].SetActive(true);
+                break;
+            case 3:
+                _posNumberArray[0].SetActive(true);
+                _nameArray[0].SetActive(true);
+                _scoreArray[0].SetActive(true);
+                _posNumberArray[1].SetActive(true);
+                _nameArray[1].SetActive(true);
+                _scoreArray[1].SetActive(true);
+                _posNumberArray[2].SetActive(true);
+                _nameArray[2].SetActive(true);
+                _scoreArray[2].SetActive(true);
+                break;
+            case 4:
+                _posNumberArray[0].SetActive(true);
+                _nameArray[0].SetActive(true);
+                _scoreArray[0].SetActive(true);
+                _posNumberArray[1].SetActive(true);
+                _nameArray[1].SetActive(true);
+                _scoreArray[1].SetActive(true);
+                _posNumberArray[2].SetActive(true);
+                _nameArray[2].SetActive(true);
+                _scoreArray[2].SetActive(true);
+                _posNumberArray[3].SetActive(true);
+                _nameArray[3].SetActive(true);
+                _scoreArray[3].SetActive(true);
+                break;
+            case 5:
+                _posNumberArray[0].SetActive(true);
+                _nameArray[0].SetActive(true);
+                _scoreArray[0].SetActive(true);
+                _posNumberArray[1].SetActive(true);
+                _nameArray[1].SetActive(true);
+                _scoreArray[1].SetActive(true);
+                _posNumberArray[2].SetActive(true);
+                _nameArray[2].SetActive(true);
+                _scoreArray[2].SetActive(true);
+                _posNumberArray[3].SetActive(true);
+                _nameArray[3].SetActive(true);
+                _scoreArray[3].SetActive(true);
+                _posNumberArray[4].SetActive(true);
+                _nameArray[4].SetActive(true);
+                _scoreArray[4].SetActive(true);
+                break;
+            case 6:
+                _posNumberArray[0].SetActive(true);
+                _nameArray[0].SetActive(true);
+                _scoreArray[0].SetActive(true);
+                _posNumberArray[1].SetActive(true);
+                _nameArray[1].SetActive(true);
+                _scoreArray[1].SetActive(true);
+                _posNumberArray[2].SetActive(true);
+                _nameArray[2].SetActive(true);
+                _scoreArray[2].SetActive(true);
+                _posNumberArray[3].SetActive(true);
+                _nameArray[3].SetActive(true);
+                _scoreArray[3].SetActive(true);
+                _posNumberArray[4].SetActive(true);
+                _nameArray[4].SetActive(true);
+                _scoreArray[4].SetActive(true);
+                _posNumberArray[5].SetActive(true);
+                _nameArray[5].SetActive(true);
+                _scoreArray[5].SetActive(true);
+                break;
+        }
+    }
+
+
+
+    private void Update()
+    {
+        AddingNewScore();
+    }
+}
