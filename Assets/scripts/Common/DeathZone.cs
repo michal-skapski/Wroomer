694b296d7ad95de192eb1a5bb22c71ba104f5317
Jankun <janmac459@gmail.com>
2020-06-18 12:43:16 +0200

    plane drops added

---
+++ Assets/scripts/Common/DeathZone.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    private float _zeroVal = 0f;
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other, _zeroVal);
+    }
+}
---
+++ Assets/scripts/Spawns/PlaneDrop.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneDrop : MonoBehaviour
+{
+    [SerializeField] private GameObject _prefab;
+    [SerializeField] private GameObject _gun;
+    [SerializeField] private int _speed = 0;
+    public void Drop()
+    {
+        if (_gun != null)
+        {
+            GameObject projectile = Instantiate(_prefab) as GameObject;
+            projectile.transform.position = transform.position + _gun.transform.forward;
+            projectile.transform.rotation = _gun.transform.rotation;
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+            rb.velocity = _gun.transform.forward * _speed;
+        }
+    }
+}
